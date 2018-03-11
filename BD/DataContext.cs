d01f5d196b061f0e4c5e636420f7dd82b5235bb1
jorgemht <jorge@jorgemht.com>
2018-03-12 00:35:02 +0100

    New models ( doctors and specialities ), added a new controlles (API and Backoffice). Configured automatic migrations. Configured the db. New Screenshots, inside the folder API

---
+++ BD/DataContext.cs
@@ -0,0 +1,22 @@
+﻿namespace BD
+{
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+
+        public DbSet<Speciality> Specialities { get; set; }
+    }
+}
---
+++ Backoffice/Controllers/SpecialitiesController.cs
@@ -0,0 +1,124 @@
+﻿namespace Backoffice.Controllers
+{
+    using Models;
+    using BD;
+    using System.Data.Entity;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    public class SpecialitiesController : Controller
+    {
+        private DataContextBackoffice db = new DataContextBackoffice();
+
+        // GET: Specialities
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Specialities.ToListAsync());
+        }
+
+        // GET: Specialities/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speciality speciality = await db.Specialities.FindAsync(id);
+            if (speciality == null)
+            {
+                return HttpNotFound();
+            }
+            return View(speciality);
+        }
+
+        // GET: Specialities/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Specialities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "SpecialityId,Name")] Speciality speciality)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Specialities.Add(speciality);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(speciality);
+        }
+
+        // GET: Specialities/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speciality speciality = await db.Specialities.FindAsync(id);
+            if (speciality == null)
+            {
+                return HttpNotFound();
+            }
+            return View(speciality);
+        }
+
+        // POST: Specialities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "SpecialityId,Name")] Speciality speciality)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(speciality).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(speciality);
+        }
+
+        // GET: Specialities/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speciality speciality = await db.Specialities.FindAsync(id);
+            if (speciality == null)
+            {
+                return HttpNotFound();
+            }
+            return View(speciality);
+        }
+
+        // POST: Specialities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Speciality speciality = await db.Specialities.FindAsync(id);
+            db.Specialities.Remove(speciality);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
